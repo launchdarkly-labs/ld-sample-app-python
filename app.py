d5d50eae466fd1ddd751182671fea8777376efbd
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2025-12-16 16:43:03 -0500

    remove ld code

--- app.py
+++ app.py
@@ -4,85 +4,17 @@ from flask import render_template
 from flask import make_response
 from flask import request
 from flask_cors import CORS
-import ldclient
-from ldclient.config import Config
-from dotenv import load_dotenv
-
-load_dotenv()
-
-with open("static/js/keys.js", "w") as f:
-    f.write('const clientKey = "' + os.environ["LD_CLIENT_KEY"] + '";')
 
 app = Flask(__name__)
 CORS(app)
 
-
-class change_tracker:
-    def __call__(self, changed):
-        print(
-            "Flag key: "
-            + str(changed.key)
-            + ", Old value: "
-            + str(changed.old_value)
-            + ", New value: "
-            + str(changed.new_value)
-        )
-
-
-changer = change_tracker()
-
-ldclient.set_config(Config(os.environ["LD_SDK_KEY"]))
-
-if ldclient.get().is_initialized():
-    print("SDK successfully initialized!")
-else:
-    print("SDK failed to initialize")
-
-mycontext = ldclient.Context.from_dict(
-    {
-        "kind": "multi",
-        "user": {
-            "key": "user-018e7bd4-ab96-782e-87b0-b1e32082b481",
-            "name": "Miriam Wilson",
-            "language": "en",
-            "tier": "premium",
-            "userId": "mwilson",
-            "role": "developer",
-            "email": "miriam.wilson@example.com",
-        },
-        "device": {
-            "key": "device-018e7bd4-ab96-782e-87b0-b1e32082b481",
-            "os": "macOS",
-            "osVersion": "15.6",
-            "model": "MacBook Pro",
-            "manufacturer": "Apple",
-        },
-    }
-)
-
-ldclient.get().flag_tracker.add_flag_value_change_listener(
-    "coffee-promo-1", mycontext, changer
-)
-ldclient.get().flag_tracker.add_flag_value_change_listener(
-    "coffee-promo-2", mycontext, changer
-)
-
-
 @app.route("/")
 def show_page():
     current_route_rule = str(request.url_rule)
-    home_page_slider = ldclient.get().variation(
-        "release-home-page-slider", mycontext, False
-    )
-    coffee_promo_1 = ldclient.get().variation("coffee-promo-1", mycontext, False)
-    coffee_promo_2 = ldclient.get().variation("coffee-promo-2", mycontext, False)
     retval = make_response(
         render_template(
             "index.html",
             current_route_rule=current_route_rule,
-            home_page_slider=home_page_slider,
-            coffee_promo_1=coffee_promo_1,
-            coffee_promo_2=coffee_promo_2,
         )
     )
     return retval
